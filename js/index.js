b9fb8359d0a3572e28cc93bd971915c813880f1b
Filippo Costa <pippoc@MacB.local>
2016-10-07 12:43:44 +0200

    Enter key detection

--- js/index.js
+++ js/index.js
@@ -1,15 +1,22 @@
+function searchTerm() {
+	var query = $(".search-bar").val();
+	$.ajax({
+		url: "https://itunes.apple.com/search",
+		type:"GET",
+		data: $.param({term:query}),
+	   	dataType: "jsonp",
+		success: function(data) {
+			var url = data["results"][0]["artworkUrl60"].replace("60x60", "1440x1440");
+			var img = $("#artwork").attr("src", url);
+		}
+	});
+};
+
 $(document).ready(function() {
-	$("#btn").on("click", function() {
-		var inputterm = $('#track').val();
-		$.ajax({
-			url: "https://itunes.apple.com/search",
-			type:"GET",
-			data: $.param({term : inputterm}),
-	    	dataType: "jsonp",
-			success: function(data) {
-				var url = data["results"][0]["artworkUrl60"].replace("60x60", "1440x1440");
-				var img = $("#artwork").attr('src', url);
-			}
-		});
+	$(".search-bar").keypress(function(e) {
+		if (e.which == 13) {
+			searchTerm();
+			return false;
+		}
 	});
 });
