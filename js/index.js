083f0eb5c14cd29b1830e0b5315a4ce35f92b274
Filippo Costa <filippocosta.italy@gmail.com>
2017-08-06 04:54:13 +0200

    Minor tweaks to the captions

--- js/index.js
+++ js/index.js
@@ -8,9 +8,13 @@ function searchTerm() {
 		success: function(data) {
 			if (data["results"].length == 0) {
 				searchError();
-			} else if (data["results"][0]["artworkUrl60"]) {
-        var url = data["results"][0]["artworkUrl60"].replace("60x60", "1440x1440");
+      } else if (data["results"][0]["artworkUrl60"]) {
+        var result = data["results"][0];
+        var url = result["artworkUrl60"].replace("60x60", "1440x1440");
         $("#artwork").attr("src", url);
+        var artist = result["artistName"] || "unknown";
+        var name = result["collectionName"] || data["results"][0]["trackName"];
+        $("#caption").text(name + " by " + artist);
       } else {
         searchError();
 			}
@@ -26,10 +30,15 @@ function searchError() {
 	$("#artwork").effect("shake", {distance:300});
 }
 
+function reset() {
+  $("#artwork").attr("src", "img/loading.gif");
+  $("#caption").text("");
+}
+
 $(document).ready(function() {
 	$("form").submit(function(event) {
     event.preventDefault();
-		$("#artwork").attr("src", "img/loading.gif");
+    reset();
     searchTerm();
 	});
 });
